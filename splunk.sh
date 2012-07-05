80e0a111fda79c35b31396acbb069e166c94c798
wangwenbin <skey@115.com>
2012-07-05 11:58:20 +0800

    更新文件

--- splunk.sh
+++ splunk.sh
@@ -1,3 +1,4 @@
+#!usr/bin
 wget -O splunk-4.3.2-123586-linux-2.6-amd64.deb 'http://www.splunk.com/index.php/download_track?file=4.3.2/splunk/linux/splunk-4.3.2-123586-linux-2.6-amd64.deb&ac=&wget=true&name=wget&typed=releases'
 dpkg -i splunk-4.3.2-123586-linux-2.6-amd64.deb 
 
